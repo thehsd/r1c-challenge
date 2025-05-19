38c117ee9dcddbb371af65327d825d3c861e4c63
alireza hosseini <hosseinialireza485@gmail.com>
2025-05-19 03:31:31 +0330

    chore: remove files

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,11 +5,6 @@ export const hasParams = (url: string): boolean => {
     : false;
 };
 
-<<<<<<< HEAD
-=======
-// ----------------------------------------------------------------------
-
->>>>>>> 9a340526c3e6c2c8d371147b716aa27a47ac5be9
 export function removeLastSlash(pathname: string): string {
   if (pathname !== "/" && pathname.endsWith("/")) {
     return pathname.slice(0, -1);
@@ -18,11 +13,6 @@ export function removeLastSlash(pathname: string): string {
   return pathname;
 }
 
-<<<<<<< HEAD
-=======
-// ----------------------------------------------------------------------
-
->>>>>>> 9a340526c3e6c2c8d371147b716aa27a47ac5be9
 export function removeParams(url: string): string {
   try {
     const urlObj = new URL(url, window.location.origin);
@@ -33,11 +23,6 @@ export function removeParams(url: string): string {
   }
 }
 
-<<<<<<< HEAD
-=======
-// ----------------------------------------------------------------------
-
->>>>>>> 9a340526c3e6c2c8d371147b716aa27a47ac5be9
 export function isExternalLink(url: string): boolean {
   return url.startsWith("http");
 }
